6c1b0a23b6d4b3c8c366ad0da77d330514563902
Zahra Kakavand <zk.nazanin@gmail.com>
2016-07-14 02:52:51 +0430

    FirstWindow

--- Code/src/FirstWindow.java
+++ Code/src/FirstWindow.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class FirstWindow extends JFrame{
@@ -8,18 +9,26 @@ public class FirstWindow extends JFrame{
 	private JPanel panel;
 	
 	public FirstWindow(){
-		setBounds(400, 200,1500, 1250);
+		//setLayout(null);
+		setTitle("خوش آمدید");
+		setSize(400,200);
+		setLocationRelativeTo(null);
 		panel = new JPanel();
+		panel.setLayout(null);
 		add(panel);
 		setVisible(true);
 		
-		loginButton = new JButton("login");
-		loginButton.setBounds(	0, 0, 80, 25);
+		loginButton = new JButton("ورود");
+		loginButton.setSize(80, 25);
+		loginButton.setLocation(150,50);
 		panel.add(loginButton);
 		
-		registerButton = new JButton("register");
-		registerButton.setBounds(10, 20, 80,25);
+		registerButton = new JButton("ثبت نام");
+		registerButton.setSize(80,25);
+		registerButton.setLocation(150,85);
 		panel.add(registerButton);
+		
+	
 	}
 	
 	
