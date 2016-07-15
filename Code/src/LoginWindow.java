03df16d585a6eee6c1614afa6ce036093023253f
Zahra Kakavand <zk.nazanin@gmail.com>
2016-07-15 13:09:26 +0430

    LoginWindow

--- Code/src/LoginWindow.java
+++ Code/src/LoginWindow.java
@@ -10,7 +10,9 @@ public class LoginWindow  extends JFrame{
 	private JButton retrieveButton;
 	private JPanel panel;
 	private JTextField usernameTextField;
-	private JTextField passwordTextField;
+	private JPasswordField passwordTextField;
+	private JLabel usernameLabel;
+	private JLabel passwordLabel;
 	
 	public LoginWindow(){
 		//setLayout(null);
@@ -21,34 +23,43 @@ public class LoginWindow  extends JFrame{
 		panel.setLayout(null);
 		add(panel);
 		setVisible(true);
+	//	setResizable(false);
 		
-		usernameTextField = new JTextField("نام کاربری");
+		usernameTextField = new JTextField();
 		usernameTextField.setSize(120, 25);
-		usernameTextField.setLocation(150,40);
+		usernameTextField.setLocation(100,10);
 		panel.add(usernameTextField);
 		
-		passwordTextField = new JTextField("کلمه عبور");
+		usernameLabel = new JLabel("نام کاربری:");
+		usernameLabel.setSize(60, 25);
+		usernameLabel.setLocation(230,10);
+		panel.add(usernameLabel);
+		
+		passwordTextField = new JPasswordField();
 		passwordTextField.setSize(120, 25);
-		passwordTextField.setLocation(150,70);
-		panel.add(passwordTextField);
+		passwordTextField.setLocation(100,40);
+		panel.add(passwordTextField); 
+		
+		passwordLabel = new JLabel("کلمه عبور:");
+		passwordLabel.setSize(60, 25);
+		passwordLabel.setLocation(230,40);
+		panel.add(passwordLabel);
 		
 		loginButton = new JButton("ورود");
 		loginButton.setSize(80, 25);
-		loginButton.setLocation(150,100);
+		loginButton.setLocation(100,70);
 		panel.add(loginButton);
 		
 		cancelButton = new JButton("انصراف");
 		cancelButton.setSize(80, 25);
-		cancelButton.setLocation(250,100);
+		cancelButton.setLocation(200,70);
 		panel.add(cancelButton);
 		
 		retrieveButton = new JButton("بازیابی نام کاربری و کلمه عبور");
 		retrieveButton.setSize(180, 25);
-		retrieveButton.setLocation(150,130);
+		retrieveButton.setLocation(100,115);
 		panel.add(retrieveButton);
-		
-		
-		
+	
 	
 	}
 	
